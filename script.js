d8a468507db28bf7598e00e5950149aa289f25d1
Yaheaalani00 <yaheaalani123@hotmail.com>
2023-10-25 19:02:52 +0200

    added the game rules

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
-document.addEventListener("DOMContentLoaded", function () {
+/**
+ * Load the DOM content
+ */
+
+document.addEventListener("DOMContentLoaded", function() {
     let wins = 0;
     let losses = 0;
 
+/**
+ * randomly get the computer's choice
+ */
     function getComputerChoice() {
-        const choices = ['rock', 'paper', 'scissors'];
-        const randomNumber = Math.floor(Math.random() * 3);
-        return choices[randomNumber];
+        let choices = ['rock', 'paper', 'scissors'];
+        let randomNumber = Math.floor(Math.random() * 3);
+        return choices[randomNumber];  
     }
 
+
+/**
+ * The games logic
+ */
+
     function playGame(playerChoice) {
-        const computerChoice = getComputerChoice();
+        let getComputerChoice = getComputerChoice();
 
-        // Display computer's choice with an image
-        const computerChoiceImage = document.getElementById('computerChoiceImage');
-        computerChoiceImage.src = `images/${computerChoice}.png`;  // Adjust the path here
-        computerChoiceImage.alt = computerChoice;
+        let getComputerChoiceImage = document.getElementById('computerChoiceImage');
+        getComputerChoiceImage.src = `images/${getComputerChoice}.png`;
+        getComputerChoiceImage.alt = getComputerChoice;
 
-        // Determine the winner
         let resultMessage = '';
 
+
+/**
+ * Game Rules
+ */
         if (
-            (playerChoice === 'rock' && computerChoice === 'scissors') ||
-            (playerChoice === 'paper' && computerChoice === 'rock') ||
-            (playerChoice === 'scissors' && computerChoice === 'paper')
+            (playerChoice === 'rock' && getComputerChoice === 'scissors') ||
+            (playerChoice === 'paper' && getComputerChoice === 'rock') ||
+            (playerChoice === 'scissors' && getComputerChoice === 'paper') ||
+        
         ) {
             resultMessage = `You win! Computer chose ${computerChoice}.`;
             wins++;
@@ -36,30 +51,8 @@ document.addEventListener("DOMContentLoaded", function () {
         } else if (playerChoice === computerChoice) {
             resultMessage = `It's a tie! Both chose ${playerChoice}.`;
         }
-
-        alert(resultMessage);
-
-        document.getElementById('correct').innerText = wins;
-        document.getElementById('incorrect').innerText = losses;
-
-        // Restart the game after the alert is closed
-        setTimeout(() => {
-            // Clear computer's choice image
-            computerChoiceImage.src = '';
-            computerChoiceImage.alt = '';
-        }, 1000); // Increase the delay if needed
     }
 
-    // Attach click event listeners to the choices
-    const rockChoice = document.getElementById('rockChoice');
-    const paperChoice = document.getElementById('paperChoice');
-    const scissorsChoice = document.getElementById('scissorsChoice');
+}
 
-    if (rockChoice && paperChoice && scissorsChoice) {
-        rockChoice.addEventListener('click', () => playGame('rock'));
-        paperChoice.addEventListener('click', () => playGame('paper'));
-        scissorsChoice.addEventListener('click', () => playGame('scissors'));
-    } else {
-        console.error('One or more choice elements not found in the DOM.');
-    }
-});
+
